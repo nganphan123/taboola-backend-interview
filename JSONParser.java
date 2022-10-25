1ef2bf3ec73b98980126be87ba1cb72e45315bf5
nganphan123 <jill01009@gmail.com>
2022-10-25 07:15:28 -0700

    add parses

--- JSONParser.java
+++ JSONParser.java
@@ -1,4 +1,5 @@
 import java.rmi.ConnectIOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +24,10 @@ public class JSONParser {
     //     json = input;
     //     return parseObject(json);
     // }
+    
+    // private static Object[] parseMixedValues(String json, int i){
+
+    // }
 
     // parse string
     private static Object[] parseString(String json, int i){
@@ -33,6 +38,68 @@ public class JSONParser {
             c = json.charAt(++i);
         }        
         return new Object[]{k,i};
+    }
+
+    private static Object[] parseArray(String json, int i){
+        // Assume all values seperated by only comma
+        ArrayList array = new ArrayList<>();
+        char c = json.charAt(++i);
+        while(c != ']'){
+            char markVal = json.charAt(i);
+            Object[] res;
+            Object v;
+            // parse string
+            if(markVal == '"'){
+                c = json.charAt(++i);
+                res = parseString(json, i);
+                v = (String)res[0];
+                i = (int)res[1];
+    
+            // parse array
+            }else if (markVal == '[') {
+                c = markVal;
+                String valArray = "";
+                while(c != ']') {
+                    valArray += c;
+                    c = json.charAt(++i);
+                }
+                valArray += c;
+                v = valArray;
+
+            // parse object
+            }else if (markVal == '{') {
+                c = markVal;
+                String valMap = "";
+                while(c != '}') {
+                    valMap += c;
+                    c = json.charAt(++i);
+                }
+                valMap += c;
+                v = parse(valMap);
+
+            // parse true, false, null, number
+            }else {
+                c = markVal;
+                String val = "";
+                while (c != ',' && c != ' ') {
+                    val += c;
+                    c = json.charAt(++i);
+                }
+                if (val.equals("true") || val.equals("false")) {
+                    v = Boolean.parseBoolean(val);
+                }else if (val.equals("null")) {
+                    v = null;
+                }else if (val.contains(".")){
+                    v = Float.parseFloat(val);
+                }else {
+                    v = Integer.parseInt(val);
+                }
+            }
+            array.add(v);
+            c = json.charAt(++i);
+        }
+        System.out.println(array);
+        return new Object[]{array,i};
     }
 
     // parse object
@@ -46,35 +113,40 @@ public class JSONParser {
             }
             String k = "";
             Object v;
+    
             // get key
             char c = json.charAt(++i);
-            while(c != '"'){
-                k += c;
-                c = json.charAt(++i);
-            }
+            Object[] res = parseString(json, i);
+            k = (String)res[0];
+            i = (int)res[1];
+    
             //skip " : "
             i+=4;
+    
             //get val
             char markVal = json.charAt(i);
+    
             // parse string
             if(markVal == '"'){
                 c = json.charAt(++i);
-                String valString = "";
-                while(c != '"') {
-                    valString += c;
-                    c = json.charAt(++i);
-                }
-                v = valString;
+                res = parseString(json, i);
+                v = (String)res[0];
+                i = (int)res[1];
+    
             // parse array
             }else if (markVal == '[') {
-                c = markVal;
-                String valArray = "";
-                while(c != ']') {
-                    valArray += c;
-                    c = json.charAt(++i);
-                }
-                valArray += c;
-                v = valArray;
+                // c = markVal;
+                // String valArray = "";
+                // while(c != ']') {
+                //     valArray += c;
+                //     c = json.charAt(++i);
+                // }
+                // valArray += c;
+                // v = valArray;
+                res = parseArray(json, i);
+                v = (ArrayList)res[0];
+                i = (int)res[1];
+
             // parse object
             }else if (markVal == '{') {
                 c = markVal;
@@ -85,6 +157,7 @@ public class JSONParser {
                 }
                 valMap += c;
                 v = parse(valMap);
+
             // parse true, false, null, number
             }else {
                 c = markVal;
--- Main.java
+++ Main.java
@@ -5,10 +5,8 @@ public class Main {
     public static void main(String[] args) {
         String json = "{ \"debug\" : \"on\", \"window\" : { \"title\" : \"sample\", \"size\" : 500 } }";
         System.out.println(JSONParser.parse(json));
-        json = "{ \"debug\" : \"on\", \"window\" : { \"title\" : \"sample\", \"size\" : 500 }, \"number\" : 600, \"decimal\" : 1.222, \"bool\" : true }";
+        json = "{ \"debug\" : \"on\", \"window\" : { \"title\" : \"sample\", \"size\" : 500 }, \"number\" : 600, \"decimal\" : 1.222, \"bool\" : true, [\"i1\",\"i2\",true,false,1] }";
         Map<String,Object> m = JSONParser.parse(json);
-        System.out.println(m.get("decimal").getClass());
-        System.out.println(m.get("bool").getClass());
-        System.out.println(m.get("window").getClass());
+        System.out.println(m);
     }
 }
